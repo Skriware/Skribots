e7371f2382a8e9ce006912995aedab9ca34998d0
adzierzanowski <a.dzierzanowski1@gmail.com>
2019-11-21 13:02:50 +0100

    class header rearrangement

--- src/utilities/SmartRotor.h
+++ src/utilities/SmartRotor.h
@@ -5,7 +5,7 @@
 
 
 /*
-  This class describes a set of two rotors with encoders
+  This class describes a single rotor with encoders
 */
 class SmartRotor
 {
@@ -21,73 +21,35 @@ class SmartRotor
     int direction;
 
     // Driver PWM output pins
-    uint8_t m1pin1;
-    uint8_t m1pin2;
-    uint8_t m2pin1;
-    uint8_t m2pin2;
+    uint8_t pin1;
+    uint8_t pin2;
 
     // Encoder inputs
-    uint8_t m1enc1;
-    uint8_t m2enc1;
-    uint8_t m1enc2;
-    uint8_t m2enc2;
+    uint8_t enc;
 
-    // Pulses per 360 deg. turn of the whole robot
-    int pulsesPerLeftTurn;
-    int pulsesPerRightTurn;
-
-    // Pulses per single rotor wheel 360 deg. turn
-    int m1pulsesPerRevolution;
-    int m2pulsesPerRevolution;
-
-    int m1pulsesPerMeter;
-    int m2pulsesPerMeter;
+    int pulsesPerRevolution;
+    int pulsesPerMeter;
 
     // No. of pulses in whichever direction since the start of the program
-    uint32_t m1pulseCount;
-    uint32_t m2pulseCount; 
+    uint32_t pulseCount;
 
     // If set to true,
-    // the flags indicate that the target is set and movement is in progress.
-    bool m1movesToTarget;
-    bool m2movesToTarget;
+    // this flag indicates that the target is set and movement is in progress.
+    bool movesToTarget;
 
-    uint32_t m1startingPulses;
-    uint32_t m2startingPulses;
-    uint32_t m1pulseTarget;
-    uint32_t m2pulseTarget;
+    uint32_t startingPulses;
+    uint32_t pulseTarget;
 
-    void m1stop(void);
-    void m2stop(void);
+    void stop(void);
 
-    static void m1encISR(void);
-    static void m2encISR(void);
+    static void encISR(void);
 
   public:
-    enum class Which : uint8_t {
-      LEFT,
-      RIGHT,
-      BOTH
-    };
-
     SmartRotor(
-      uint8_t m1pin1,
-      uint8_t m1pin2,
-      uint8_t m2pin1,
-      uint8_t m2pin2,
-      uint8_t m1enc1,
-      uint8_t m1enc2,
-      uint8_t m2enc1,
-      uint8_t m2enc2
+      uint8_t pin1,
+      uint8_t pin2,
+      uint8_t enc1,
     );
-
-    SmartRotor(
-      uint8_t m1pin1,
-      uint8_t m1pin2,
-      uint8_t m2pin1,
-      uint8_t m2pin2,
-      uint8_t m1enc1,
-      uint8_t m2enc1);
     
     ~SmartRotor(void);
 
@@ -96,21 +58,14 @@ class SmartRotor
     void setSpeed(int speed);
     void setDirection(int direction);
     void setPulsesPerTurn(int pulsesPerTurn);
-    void setPulsesPerTurn(int pulsesPerLeftTurn, int pulsesPerRightTurn);
     void setPulsesPerMeter(int pulsesPerMeter);
-    void setPulsesPerMeter(int m1pulsesPerMeter, int m2pulsesPerMeter);
     void setPulsesPerRevolution(int pulsesPerRevolution);
-    void setPulsesPerRevolution(
-      int m1pulsesPerRevolution, int m2pulsesPerRevolution);
 
     void move(void);
     // Move until a certain number of pulses is reached
-    void moveByPulses(int m1pulses, int m2pulses); 
-    void moveByMeters(float meters, Which rotor=Which::BOTH);
-    void moveByRevolutions(float revolutions, Which rotor=Which::BOTH);
-    void turn(bool clockwise);
-    void turnByAngle(int angle);
-    void turnByRevolutions(float revolutions);
+    void moveByPulses(int pulses);
+    void moveByMeters(float meters);
+    void moveByRevolutions(float revolutions);
     void stop(void);
     bool isMoving(void);
 };
